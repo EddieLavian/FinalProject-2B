46e52f2fb4e9630add05276d05d528529e3fbc4e
Eddie Lavian <eddielavian1@gmail.com>
2017-12-17 16:29:28 +0200

    upload all

---
+++ app/src/main/java/com/example/talyeh3/myapplication/Chat/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.talyeh3.myapplication.Chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import com.example.talyeh3.myapplication.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_chat );
+    }
+}
