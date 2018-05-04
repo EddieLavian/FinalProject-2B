eb6a6bf3451860d14e6efdf97010bec0e1ce4c1e
Eddie Lavian <eddielavian1@gmail.com>
2018-05-04 09:44:56 +0300

    Uploaded app Changing package name

--- app/src/androidTest/java/com/tobe/talyeh3/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tobe/talyeh3/myapplication/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.talyeh3.myapplication;
+package com.tobe.talyeh3.myapplication;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -15,13 +15,13 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class tobeInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.talyeh3.myapplication", appContext.getPackageName());
+        assertEquals("com.tobe.talyeh3.myapplication", appContext.getPackageName());
     }
 }
 
